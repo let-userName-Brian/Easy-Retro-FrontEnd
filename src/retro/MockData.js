5bb708609f6e3196e97b826e3a6f902d38c03e38
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-18 18:29:57 -0500

    add more retros

--- src/retro/MockData.js
+++ src/retro/MockData.js
@@ -8,12 +8,25 @@ export const mock_users = [
   { user_id: "e8c00cd6-4077-470f-8b61-17a4a1386959", user_name: 'James' }
 ]
 
-export const mock_retro = {
-  retro_id: "e0fef645-088d-4f13-b53a-ccb95f4f2131",
-  retro_name: "Chasten's Thoughts on Mjolnir",
-  column_ids: JSON.stringify([1, 2, 3]),
-  tags: ["aypples", "banaynays"]
-}
+export const mock_retros = [
+  {
+    retro_id: "e0fef645-088d-4f13-b53a-ccb95f4f2131",
+    retro_name: "Chasten's Thoughts on Mjolnir",
+    column_ids: JSON.stringify([1, 2, 3]),
+    tags: ["aypples", "banaynays"]
+  },
+  {
+    retro_id: "78557390-b5bb-4a3f-80d2-795814f99f66",
+    column_ids: JSON.stringify([4, 5, 6]),
+    tags: ["ooples", "banoonoos"]
+  },
+  {
+    retro_id: "9202ffb1-7086-4ac0-9f5a-597fcf620429",
+    retro_name: "Some More Froots",
+    column_ids: JSON.stringify([7, 8, 9]),
+    tags: ["ipples", "baninis"]
+  }
+]
 
 export const mock_columns = () => {
   let n = 1
