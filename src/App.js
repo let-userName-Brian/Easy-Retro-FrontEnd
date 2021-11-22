e394476cf97a9ce7232342babd470ae37b986f62
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-22 18:18:01 -0500

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme } from '@mui/material/styles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Route, Routes
 } from "react-router-dom";
