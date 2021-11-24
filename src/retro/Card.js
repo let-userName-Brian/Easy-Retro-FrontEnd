b246b20e70c8a92c4dcff4c8fcaea874f5bc203d
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-24 10:47:42 -0500

    fix prop naming

--- src/retro/Card.js
+++ src/retro/Card.js
@@ -1,31 +1,31 @@
 //import Comment from "./Comment.js"
 
-export default function Card({key:card_id}){
+export default function Card({ card_id }) {
 
-    // useEffect(() => {
-    //     // Received when the server sends us a card
-    //     socket.on('receivedCards', (cards) => setCards(cards))
-    //     socket.on('updatedColumnName', (column) => {
-    //       if (column.column_id === column_id) {
-    //         setColumnName(column.column_name)
-    //       }})
-    //       socket.on('updatedColumnCardIds', (column) => {
-    //         if (column.column_id === column_id) {
-    //           setColumnCardIds(column.card_ids)
-    //     }})
-    //   }, [column_id])
+  // useEffect(() => {
+  //     // Received when the server sends us a card
+  //     socket.on('receivedCards', (cards) => setCards(cards))
+  //     socket.on('updatedColumnName', (column) => {
+  //       if (column.column_id === column_id) {
+  //         setColumnName(column.column_name)
+  //       }})
+  //       socket.on('updatedColumnCardIds', (column) => {
+  //         if (column.column_id === column_id) {
+  //           setColumnCardIds(column.card_ids)
+  //     }})
+  //   }, [column_id])
 
-    return (
+  return (
     <ul>
-        <div> Card ID: {card_id}</div>
-        {/* <div> Card Text {card_text}</div>
+      <div> Card ID: {card_id}</div>
+      {/* <div> Card Text {card_text}</div>
         <div> User ID: {user_id}</div>
         <div> Votes: {votes}</div> */}
-        {/* {comments.map((comment) => (
+      {/* {comments.map((comment) => (
             <Comment comment={comment} key={comment.comment_id} />
         ))} */}
     </ul>
-    )
+  )
 }
 // need card ID from column where column ID = card ID
 // pass in column ID as props
