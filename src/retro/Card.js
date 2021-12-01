3d3abc088b2ab6e7407432966d45badb89e3bc89
Mario <matthew.davies.3@spaceforce.mil>
2021-12-01 15:17:57 -0500

    add cards

--- src/retro/Card.js
+++ src/retro/Card.js
@@ -8,39 +8,53 @@ import RecommendIcon from '@mui/icons-material/Recommend';
 
 
 
-export default function Card({ c }) {
-  const { comments: initComments, vote_type, user_id, userVotes, setUserVotes} = useContext(RetroContext)
-  const [card, setCard] = useState(c)
-  const [cardText, setCardText] = useState(card.card_text)
-  const [author, setAuthor] = useState(card.user_name)//needs to be converted to user_name
-  const [cardVotes, setCardVotes] = useState([]) 
+export default function Card({ card_id, cards }) {
+  const { comments: initComments, vote_type, user_id, userVotes, setUserVotes } = useContext(RetroContext)
+  const [card, setCard] = useState()
+  const [cardText, setCardText] = useState()
+  const [author, setAuthor] = useState()//needs to be converted to user_name
+  const [cardVotes, setCardVotes] = useState([])
   const [voted, setVoted] = useState(false)
-  const [comments, setComments] = useState(initComments.filter(comment => comment.card_id === card.card_id))
+  const [comments, setComments] = useState([])
 
- console.log("userVotes", userVotes ) // set from the context of the payload @ max_votes
+  //console.log("userVotes", userVotes ) // set from the context of the payload @ max_votes
 
-useEffect(() => {
-  socket.on('votesChanged', ({card_id, vote_type}) => {
-    if(card_id === card.card_id) {
-    setCardVotes(cardVotes)
+  useEffect(() => {
+    if (!cards) return;
+    if (cards.length === 0) return;
+    if (!card_id) return;
+    console.log('cards', cards)
+    let newCard = cards.find(card => card.card_id === card_id)
+    if (!newCard) return;
+    setCard(newCard)
+    setCardText(newCard.card_text)
+    setAuthor(newCard.user_name)
+    setComments(initComments.filter(comment => comment.card_id === card.card_id))
+  }, [cards, card_id]);
+
+
+  useEffect(() => {
+    socket.on('votesChanged', ({ card_id, vote_type }) => {
+      if (card_id === card.card_id) {
+        setCardVotes(cardVotes)
+      }
+    })
+    return () => {
+      socket.off('votesChanged')
     }
-  })
-  return () => {
-    socket.off('votesChanged')
-  }
   }, [])
 
-  console.log('cardVotes', cardVotes)
- 
+  //console.log('cardVotes', cardVotes)
 
-//add vote to card
-  const addVote = ()=> {
+
+  //add vote to card
+  const addVote = () => {
     if (userVotes > 0) {
-    console.log("vote up")
-    socket.emit('addVote', { card_id: card.card_id, vote_type: 'up', user_id }) 
-    setVoted(true)
-    setCardVotes(cardVotes.concat({ user_id, vote_type: 'up' }))
-    setUserVotes(userVotes - 1)
+      console.log("vote up")
+      socket.emit('addVote', { card_id: card.card_id, vote_type: 'up', user_id })
+      setVoted(true)
+      setCardVotes(cardVotes.concat({ user_id, vote_type: 'up' }))
+      setUserVotes(userVotes - 1)
     }
   }
 
@@ -52,7 +66,7 @@ useEffect(() => {
     setCardVotes(cardVotes.filter(v => v.user_id !== user_id))
     setUserVotes(userVotes + 1)
   }
-  
+
 
   return (
     <Box
@@ -68,16 +82,16 @@ useEffect(() => {
           inputProps: { style: { textAlign: "center" } }
         }} />
         <Box sx={{ m: 1 }}>
-        <Typography>-{author}</Typography>
-        {userVotes === 0 
-        ? <Typography >Voting isn't enabled</Typography>
-        : <Typography >Votes: {cardVotes.length}
-          {voted  
-          ? <Button onClick={() => removeVote()}>Vote cast! Remove?</Button> 
-          : <Button onClick={() => addVote()}><RecommendIcon /></Button>
-          }
-        </Typography>}
-        {comments.map((comment) => (<Comment key={comment.comment_id} comment={comment} />))}
+          <Typography>-{author}</Typography>
+          {userVotes === 0
+            ? <Typography >Voting isn't enabled</Typography>
+            : <Typography >Votes: {cardVotes.length}
+              {voted
+                ? <Button onClick={() => removeVote()}>Vote cast! Remove?</Button>
+                : <Button onClick={() => addVote()}><RecommendIcon /></Button>
+              }
+            </Typography>}
+          {comments.map((comment) => (<Comment key={comment.comment_id} comment={comment} />))}
         </Box>
       </Paper>
     </Box>
