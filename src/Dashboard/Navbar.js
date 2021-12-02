45ba128a48d035fc670deb3a85c267496fd5c8e3
Brian <brian.hardy67@gmail.com>
2021-12-02 14:29:32 -0500

    Timer fixes

--- src/Dashboard/Navbar.js
+++ src/Dashboard/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar({ user, darkMode, setDarkMode }) {
   }
 
   return (
-    <Box sx={{ flexGrow: 1, color: 'inherit', width: '100vw', overflow: 'hidden', /*adjust as the screen adjusts*/ }}>
+    <Box sx={{ flexGrow: 1, color: 'inherit', width: '100vw', overflow: 'hidden' }}>
       <AppBar position="static" style={{ backgroundColor: '#121212', height: 80 }}>
         <Toolbar>
           <div style={{ flexGrow: 1, m: 0, p: 0 }}>
