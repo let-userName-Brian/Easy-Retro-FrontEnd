9d5faf6c6bbccc32d3b3b26eabb7796603d239f7
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-12-02 14:54:23 -0500

    add card button tooltip

--- src/retro/AddCardButton.js
+++ src/retro/AddCardButton.js
@@ -1,8 +1,8 @@
-import { Box, Icon, IconButton } from '@mui/material/';
+import { Box, Icon, IconButton, Tooltip } from '@mui/material/';
 
 export default function AddCardButton({ addCardFunc }) {
   return (
     <Box sx={{ width: 60, mx: 'auto' }}>
-      <IconButton onClick={addCardFunc}><Icon fontSize='large'>add_circle</Icon></IconButton>
+      <Tooltip title='Add Card'><IconButton onClick={addCardFunc}><Icon fontSize='large'>add_circle</Icon></IconButton></Tooltip>
     </Box >)
 }
