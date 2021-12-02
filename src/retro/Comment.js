f298f64a6538644e1829a01b960bb68b40a17083
Mario <matthew.davies.3@spaceforce.mil>
2021-12-02 08:51:35 -0500

    add socket on

--- src/retro/Comment.js
+++ src/retro/Comment.js
@@ -7,18 +7,9 @@ import { RetroContext } from "./Retro";
 
 export default function Comment({ comment_id, comment, user_id, retro_id, user }) {
 
+  const { comments: initComments } = useContext(RetroContext)
   const [commentText, setCommentText] = useState('')
   const [author, setAuthor] = useState('')
-  // const [reactions, setReactions] = useState([])
-  const { comments: initComments } = useContext(RetroContext)
-
-  // console.log("comment ID:",comment_id)
-
-
-  function changeCommentText() {
-    socket.emit('changeCommentText', { comment_id })
-  }
-
 
   useEffect(() => {
     let comment = initComments?.find(c => c.comment_id === comment_id)
@@ -31,21 +22,68 @@ export default function Comment({ comment_id, comment, user_id, retro_id, user }
     // setReactions(comment.reactions)
   }, [initComments, comment_id])
 
+  useEffect(() => {
+    socket.on('commentTextUpdated', ({ comment }) => {
+      if (comment_id === comment.comment_id) {
+        // setComment(comment);
+        setCommentText(comment.comment_text)
+      }
+    })
 
-  //send to Dustin--------------------------------------------------------------------
-  function removeCommentFunc() {
-    console.log('remove comment id:', comment_id)
-    //socket.emit('removeComment', { comment_id })
+    return () => {
+      socket.off('commentTextUpdated')
+    }
+  }, [comment_id])
+
+  function removeComment() {
+    let card_id = comment.card_id
+    console.log('removeComment:', comment_id, card_id)
+    socket.emit('removeColumn', { comment_id, card_id })
   }
 
-  //submit comment to backend for DUSTINNNNN--------------------------------------------------------------------
-  function submitCommentFunc() {
-    console.log('submit comment id:', comment_id)
-    //socket.emit('submitComment', { card_id, comment_text: commentText, user_id })
+  function changeCommentText() {
+    socket.emit('changeCommentText', { comment_id })
   }
+
+
+
+  // //send to Dustin--------------------------------------------------------------------
+  // function removeCommentFunc() {
+  //   console.log('remove comment id:', comment_id)
+  //   //socket.emit('removeComment', { comment_id })
+  // }
+
+  // //submit comment to backend for DUSTINNNNN--------------------------------------------------------------------
+  // function submitCommentFunc() {
+  //   console.log('submit comment id:', comment_id)
+  //   //socket.emit('submitComment', { card_id, comment_text: commentText, user_id })
+  // }
 
   //coment Id will be sent back from DB after initialization of comments
   //we need where user Id and comment ID match.....set that as Author
+
+
+
+  //just here for strucrture reference
+  // function removeColumn() {
+  //   console.log('del col:', retro_id, column_id)
+  //   socket.emit('removeColumn', { retro_id, column_id })
+  // }
+
+  // function renameColumn() {
+  //   // let retro_id = retro.retro_id;
+  //   let column_id = column.column_id
+  //   socket.emit('renameColumn', { retro_id, column_id, column_name: colName })
+  // }
+
+  // function addCard(column_id) {
+  //   socket.emit('addCard', { retro_id, column_id, user_id });
+  // }
+
+  //do we need this?
+  // if (!column) {
+  //   return <>Loading Column</>
+  // }
 
   return (
     <Paper variant="outlined" sx={{ m: 1, p: 1, borderRadius: '15px' }} >
@@ -53,7 +91,7 @@ export default function Comment({ comment_id, comment, user_id, retro_id, user }
       <TextField fullWidth multiline id="commentText" value={commentText} onChange={(e) => setCommentText(e.target.value)} onBlur={changeCommentText} sx={{ my: 1 }} />
       <Box>
         <Typography >Author: {author}</Typography>
-        <CommentMenu removeCommentFunc={removeCommentFunc} />
+        <CommentMenu removeCommentFunc={removeComment} />
         {author ? <Button onClick={() => console.log('submit comment')}>Submit Comment</Button>
           : <></>}
       </Box>
