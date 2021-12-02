deeb7db80464265d1bad6c6af2e7f4ae20013398
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-12-01 21:14:50 -0500

    move onClick to parent button

--- src/retro/AddCardButton.js
+++ src/retro/AddCardButton.js
@@ -3,6 +3,6 @@ import { Box, Icon, IconButton } from '@mui/material/';
 export default function AddCardButton({ addCardFunc }) {
   return (
     <Box sx={{ width: 60, mx: 'auto' }}>
-      <IconButton><Icon fontSize='large' onClick={addCardFunc}>add_circle</Icon></IconButton>
-    </Box>)
+      <IconButton onClick={addCardFunc}><Icon fontSize='large'>add_circle</Icon></IconButton>
+    </Box >)
 }
